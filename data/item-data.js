3819441d13522c4919661941d2a764dd63560806
Abenad1123 <103361048+Abenad1123@users.noreply.github.com>
2026-07-23 23:12:23 +0800

    Index Update 0.15: Added basic inventory functions and some assets

--- data/item-data.js
+++ data/item-data.js
@@ -3,33 +3,44 @@ setup.item = {
     1001: {
         name: "Green Apple",
         stackLimit: 64,
+        category: "Consumable",
+        subCategory: "Food",
         tags: [],
         description: "",
         img: "",
-        stats: {}
+        stat: {}
     },
     1002: {
         name: "Wooden Stick",
         stackLimit: 64,
+        category: "Material",
+        subCategory: "Wood",
         tags: [],
         description: "",
         img: "",
-        stats: {}
+        stat: {}
     },
     1003: {
         name: "Iron Ore",
         stackLimit: 64,
+        category: "Material",
+        subCategory: "Mineral",
         tags: [],
         description: "",
         img: "",
-        stats: {}
+        stat: {}
     },
     1004: {
         name: "Wood Short Sword",
         stackLimit: 1,
+        category: "Weapon",
+        subCategory: "Sword",
         tags: [],
         description: "",
         img: "",
-        stats: {}
+        stat: {
+            durability: 20,
+            weight: 2
+        }
     },
 }
