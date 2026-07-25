f820627b297828c68312056a567b526a273b65b3
Abenad1123 <103361048+Abenad1123@users.noreply.github.com>
2026-07-25 14:20:05 +0800

    Indev Update 0.17: Character Framework Update
    ### Added
    
    * Character trait removal.
    * Character title assignment and removal.
    * Area navigation history (the game now tracks explored areas).
    * Two innate traits.
    * One character title.
    
    ### Improved
    
    * Trait data structure.
    * Player character (PC) data structure.
    * Character information display in the Character passage.
    
    ### Fixed
    
    * Various bugs.
    * Variable naming consistency.

---
+++ data/title-data.js
@@ -0,0 +1,14 @@
+setup.title = {
+    1001: {
+        name: "Sword Master",
+        description: "",
+        img: "",
+        modifier: {
+            str: 5,
+            con: 5,
+            int: 5,
+            agi: 5,
+            per: 5
+        }
+    }
+}
--- data/trait-data.js
+++ data/trait-data.js
@@ -1,12 +1,34 @@
 setup.trait = {
     1001: {
         name: "Thick Bones",
-        type: "Acquired",
+        type: "Innate",
+        rank: "Common",
         description: "",
         img: "",
-        effect: {
+        modifier: {
             str: 1,
             con: 5
         }
-    }
+    },
+    1002: {
+        name: "Sharp Eyes",
+        type: "Innate",
+        rank: "Common",
+        description: "",
+        img: "",
+        modifier: {
+            per: 5,
+        }
+    },
+    1002: {
+        name: "Nible Feet",
+        type: "Innate",
+        rank: "Common",
+        description: "",
+        img: "",
+        modifier: {
+            agi: 3,
+            per: 3
+        }
+    },
 }
