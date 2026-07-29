da74ff5e6052e273a86142b3f8b1dcd1aa98ac1d
Abenad1123 <103361048+Abenad1123@users.noreply.github.com>
2026-07-29 22:45:32 +0800

    Indev Update 0.20: Feats and save system
    ### Added
    * Added 1 trait
    * Added description on all traits
    * Basic custom character save system (Save, delete, and load)
    * Added global feats/achievements
     - When your character achieves something, it is saved on local storage and a save file. This data are compiled with the feats of your other characters. This feature will be used in character creation.
    
    ### Improved
    * Improved file path
    
    ### Fixes
    * Removed unused asset
    * Animation for images of a scene

--- data/trait-data.js
+++ data/trait-data.js
@@ -3,7 +3,7 @@ setup.trait = {
         name: "Thick Bones",
         type: "Innate",
         rank: "Common",
-        description: "",
+        description: "You are born with bones thicker than normal",
         img: "",
         modifier: {
             str: 1,
@@ -14,21 +14,35 @@ setup.trait = {
         name: "Sharp Eyes",
         type: "Innate",
         rank: "Common",
-        description: "",
+        description: "You are born with eyes that see farther than the ordinary",
         img: "",
         modifier: {
             per: 5,
         }
     },
-    1002: {
+    1003: {
         name: "Nible Feet",
         type: "Innate",
         rank: "Common",
-        description: "",
+        description: "Your feet are more flexible and possess more control",
         img: "",
         modifier: {
             agi: 3,
             per: 3
         }
     },
+    1003: {
+        name: "Perfect Genes",
+        type: "Innate",
+        rank: "Rare",
+        description: "You are born with genes of high calibre far suprassing the ordinary giving far more power and strength",
+        img: "",
+        modifier: {
+            str: 5,
+            con: 5,
+            int: 5,
+            agi: 5,
+            per: 5
+        }
+    }
 }
